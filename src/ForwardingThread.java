58afe998c87fe981fd1aba99cf31db20ed68d68b
Devin Schwab <digidevin@gmail.com>
2012-10-27 00:24:13 -0400

    Added basic skeleton of ForwardingThread

---
+++ src/ForwardingThread.java
@@ -0,0 +1,29 @@
+
+public class ForwardingThread implements Runnable {
+	/*
+	 * Class: ForwardingThread
+	 * 
+	 * Author: Devin Schwab
+	 * 
+	 * Case ID: dts34
+	 * 
+	 * Created: 10/26/12
+	 * 
+	 * Description
+	 * ------------
+	 * This class is used to forward data from
+	 * one socket to another.
+	 */
+
+	public ForwardingThread()
+	{
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
